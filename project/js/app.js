a6f9d5e63f244eb65dc9eef54cc7a115ea562964
suraevamv@yandex.ru <suraevamv@yandex.ru>
2016-07-08 14:12:14 +0400

    реализован вывод данных с сервера в виде таблицы по нажатию на кнопку "Загрузить расписание"

--- project/js/app.js
+++ project/js/app.js
@@ -5,12 +5,20 @@ function loadlist() {
   xhr.open('GET', 'http://private-1a128-timetable4.apiary-mock.com/schedule-items', false);
   xhr.send();
 
-  if (xhr.status != 200) {
-    // обработать ошибку
-    alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
-  } else {
-    // вывести результат
-    //alert(xhr.responseText);
-    document.getElementById('ajax').innerHTML=xhr.responseText; 
+  // если нет ошибки, вывести результат
+  if (xhr.status == 200) {
+    var data = JSON.parse(xhr.responseText);
+
+    var output = '';
+    for (var i in data.data) {
+        output+='<div class="row">' +
+        '<div class="small-1 columns">' + " " + '<input type="checkbox" id="' + data.data[i].id + '">' + " " + '</div>' + " " +
+        '<div class="small-2 columns">' + " " + data.data[i].attributes.time + " " + '</div>' + " " +
+        '<div class="small-6 columns">' + " " + data.data[i].attributes.title + " " + '</div>' + " " +
+        '<div class="small-2 columns"><button type="button" class="tiny alert button" name="' + data.data[i].id + '">' + " " + 'x</button></div>' + " " +
+        "</div>";
+    }
+
+    document.getElementById("ajax").innerHTML=output;
   }
 }
