b04254a8cbde4196106f3bee0edf0ae4bb595a79
suraevamarina <suraevamv@yandex.ru>
2016-07-13 17:06:10 +0400

    изменен вывод расписания

--- project/app/components/ListContainer.js
+++ project/app/components/ListContainer.js
@@ -2,6 +2,7 @@ var React = require('react');
 var AddItem = require('./AddItem');
 var AppStore = require('../stores/AppStore');
 var AppActions = require('../actions/AppActions');
+var ScheduleItem = require('./ScheduleItem');
 
 var ListContainer = React.createClass({
   getInitialState: function(){
@@ -10,47 +11,52 @@ var ListContainer = React.createClass({
     }
   },
   componentDidMount: function(){
+    // слушает событие и обновляет, если событие произошло
     AppStore.addChangeListener(this._onChange);
   },
   componentWillUnmount: function(){
+    // больше не слушает событие
     AppStore.removeChangeListener(this._onChange);
   },
   handleAddItem: function(newName,newTime){
     AppActions.addItem(newName,newTime);
   },
+  // при изменении получаем новый список
   _onChange: function(){
     this.setState({
       list: AppStore.getList()
     })
   },
-  // Функция вывода данных, полученных через api
-loadlist: function(){
+loaddata: function() {
   var xhr = new XMLHttpRequest();
 
   xhr.open('GET',
   'http://private-1a128-timetable4.apiary-mock.com/schedule-items',false);
   xhr.send();
 
-  // если нет ошибки, вывести результат
-  if (xhr.status == 200) {
+  if (xhr.status == 200)
     var data = JSON.parse(xhr.responseText);
 
-    var output = '';
-    for (var i in data.data) {
-        output+='<div class="row">' +
-        '<div class="small-1 columns">' + " " + '<input type="checkbox" id="' + data.data[i].id + '">' + " " + '</div>' + " " +
-        '<div class="small-2 columns">' + " " + data.data[i].attributes.time + " " + '</div>' + " " +
-        '<div class="small-6 columns">' + " " + data.data[i].attributes.title + " " + '</div>' + " " +
-        '<div class="small-2 columns"><button type="button" class="tiny alert button" name="' + data.data[i].id + '">' + " " + 'x</button></div>' + " " +
-        "</div>";
-    }
-   document.getElementById('ajax').innerHTML=output;
- }
+  var output = data.data;
+
+  return output;
 },
 openbox: function() {
   document.getElementById('box').style.display='block';
 },
   render: function(){
+    var template;
+
+    var _data = this.loaddata();
+
+    template = _data.map(function(item,index) {
+      return (
+        <div key={index}>
+          <ScheduleItem data = {item} />
+        </div>
+      )
+    })
+
     return (
       <div className="row">
         <div className="large-12 column">
@@ -58,14 +64,13 @@ openbox: function() {
 
           <div className="large-4 column">
               <div className="expanded button-group">
-                <a className="button" onClick={this.loadlist}>Загрузить расписание</a>
                 <a className="button" onClick={this.openbox}>Добавить позицию</a>
               </div>
-              <p id="ajax"></p>
+
+              <p> {template} </p>
           </div>
 
           <AddItem add={this.handleAddItem}/>
-        
 
         </div>
       </div>
