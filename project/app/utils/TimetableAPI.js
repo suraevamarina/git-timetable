76a223c8c540671f267a850fc1bb81142a9fed22
suraevamarina <suraevamv@yandex.ru>
2016-07-14 20:13:19 +0400

    реализован запрос на добавление нового пункта

--- project/app/utils/TimetableAPI.js
+++ project/app/utils/TimetableAPI.js
@@ -14,5 +14,18 @@ module.exports = {
     var output = data.data;
 
     return output;
-  }
+  },
+  // добавление нового элемента
+  addItem: function(data) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('POST',
+    'http://private-1a128-timetable4.apiary-mock.com/schedule-items',false);
+    xhr.send();
+
+    if (xhr.status == 200)
+      return true;
+    else
+      return false;
+  }  
 };
