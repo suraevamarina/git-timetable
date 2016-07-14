5f1d04dd564bac95bdf429ab68e371097cb17fa0
suraevamarina <suraevamv@yandex.ru>
2016-07-14 12:29:17 +0400

    добавлен action для получения исходных данных

--- project/app/actions/AppActions.js
+++ project/app/actions/AppActions.js
@@ -2,10 +2,17 @@ var AppDispatcher = require('../dispatcher/AppDispatcher');
 var appConstants = require('../constants/appConstants');
 
 var AppActions = {
-  addItem: function(name,time){
+  // получение начальных данных
+  receiveData: function(data) {
+    AppDispatcher.handleAction({
+      actionType: appConstants.RECEIVE_DATA,
+      data: data
+    })
+  },
+  addItem: function(data){
     AppDispatcher.handleAction({
       actionType: appConstants.ADD_ITEM,
-      data: {name,time}
+      data: data
     });
   }
 };
