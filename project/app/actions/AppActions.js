c1d36db26b51535df083f81ac98e725d4f0868cf
suraevamarina <suraevamv@yandex.ru>
2016-07-14 17:45:52 +0400

    добавлен вызов метода получения данных с сервера

--- project/app/actions/AppActions.js
+++ project/app/actions/AppActions.js
@@ -1,9 +1,13 @@
 var AppDispatcher = require('../dispatcher/AppDispatcher');
 var appConstants = require('../constants/appConstants');
+var TimetableAPI = require('../utils/TimetableAPI');
 
 var AppActions = {
   // получение начальных данных
-  receiveData: function(data) {
+  receiveData: function() {
+    // получение данных через api
+    var data = TimetableAPI.getItems();
+
     AppDispatcher.handleAction({
       actionType: appConstants.RECEIVE_DATA,
       data: data
