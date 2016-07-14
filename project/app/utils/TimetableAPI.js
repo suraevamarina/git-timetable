a6fae6d2812fc986b1265e9f9594670dcf52dd9f
suraevamarina <suraevamv@yandex.ru>
2016-07-14 12:15:50 +0400

    запрос на получение данных с сервера вынесен в отдельный файл

---
+++ project/app/utils/TimetableAPI.js
@@ -0,0 +1,20 @@
+var AppActions = require('../actions/AppActions');
+
+module.exports = {
+
+  // получение данных с сервера
+  getItems: function() {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET',
+    'http://private-1a128-timetable4.apiary-mock.com/schedule-items',false);
+    xhr.send();
+
+    if (xhr.status == 200)
+      var data = JSON.parse(xhr.responseText);
+
+    var output = data.data;
+
+    AppActions.receiveData(output);
+  }
+};
