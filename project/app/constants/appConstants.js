04802d30120a17e734a002a1d4bd8f5238324fa0
suraevamarina <suraevamv@yandex.ru>
2016-07-19 22:17:06 +0400

    добавлена константа UPDATE_ITEM

--- project/app/constants/appConstants.js
+++ project/app/constants/appConstants.js
@@ -1,6 +1,8 @@
 var appConstants = {
   ADD_ITEM: "ADD_ITEM",
-  RECEIVE_DATA: "RECEIVE_DATA"
+  RECEIVE_DATA: "RECEIVE_DATA",
+  REMOVE_ITEM: "REMOVE_ITEM",
+  UPDATE_ITEM: "UPDATE_ITEM"
 };
 
 module.exports = appConstants;
