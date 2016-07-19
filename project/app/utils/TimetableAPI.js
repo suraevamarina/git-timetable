4103e0e59a28c8aa135147ad5f995bd215f444a3
suraevamarina <suraevamv@yandex.ru>
2016-07-19 22:19:09 +0400

    добавлен запрос на обновление данных

--- project/app/utils/TimetableAPI.js
+++ project/app/utils/TimetableAPI.js
@@ -1,31 +1,31 @@
 module.exports = {
 
   // получение данных с сервера
-  getItems: function(callback) {
-    var xhr = new XMLHttpRequest();
+    getItems: function(callback) {
+      var xhr = new XMLHttpRequest();
 
-    xhr.open('GET',
-    'http://private-1a128-timetable4.apiary-mock.com/schedule-items');
+      xhr.open('GET',
+      'http://private-1a128-timetable4.apiary-mock.com/schedule-items');
 
-    xhr.onreadystatechange = function() {
-      if (this.readyState == 4) {
-        if (this.status != 200) {
-          // обработать ошибку
-          alert("Данные не были получены!");
-          return;
-        }
-        else {
-          var data = JSON.parse(this.responseText);
+      xhr.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status != 200) {
+            // обработать ошибку
+            alert("Данные не были получены!");
+            return;
+          }
+          else {
+            var data = JSON.parse(this.responseText);
 
-          var output = data.data;
+            var output = data.data;
 
-          callback(output);
+            callback(output);
+          }
         }
       }
-    }
 
-    xhr.send();
-  },
+      xhr.send();
+    },
   // добавление нового элемента
   addItem: function(data,callback) {
     var json = JSON.stringify(data);
@@ -73,5 +73,33 @@ module.exports = {
       }
     }
     xhr.send();
-  }
+  },
+  // обновление элемента
+  updateItem: function(id,time,title,callback) {
+    data = {"id": id, "attributes": {"title": title, "time": time}};
+
+    var json = JSON.stringify(data);
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('PUT',
+    'http://private-1a128-timetable4.apiary-mock.com/schedule-items/$id');
+
+    xhr.setRequestHeader('Content-Type', 'application/json', 'charset=utf-8');
+
+    xhr.onreadystatechange = function() {
+      if (this.readyState == 4) {
+        if (this.status != 200) {
+          // обработать ошибку
+          alert("Данные не были обновлены!");
+          callback(false);
+          return;
+        }
+        else {
+          callback(true);
+        }
+      }
+    }
+    xhr.send(json);
+  },
 };
