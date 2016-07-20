07ddb3156effca3dcecb395a417f1230ff710e39
suraevamarina <suraevamv@yandex.ru>
2016-07-20 22:01:57 +0400

    добавлено подсвечивание строки при выборе времени

--- project/app/components/TimeBlock.js
+++ project/app/components/TimeBlock.js
@@ -10,6 +10,30 @@ var TimeBlock = React.createClass({
   setTime: function(time){
     var a = document.getElementsByTagName('input');
     for (var i = 0; i < a.length; i++) {
+        var divTime = a[i].id + '_time',
+            divTitle = a[i].id + '_title',
+            divCancel = a[i].id + '_btnCancel';
+
+      if (document.getElementById(divTime) != null)
+      {
+        var content_time = document.getElementById(divTime).innerHTML;
+        if (content_time == time)
+        {
+          var computedStyle = getComputedStyle(document.getElementById(divTitle));
+
+          // если пункты не были подсвечены
+          if (computedStyle.backgroundColor == "rgb(247, 180, 168)")
+          {
+            // подсвечиваем
+            document.getElementById(divTitle).style.background = "#ec5840";
+          }
+          // если пункты уже подсвечены
+          else if (computedStyle.backgroundColor == "rgb(236, 88, 64)")
+          {
+            // убираем подсветку
+            document.getElementById(divTitle).style.background = "#f7b4a8";
+          }
+
         // если чекбокс отмечен
         if ((a[i].type == 'checkbox') && (a[i].checked)) {
           var list = this.getList();
@@ -23,17 +47,21 @@ var TimeBlock = React.createClass({
             }
           }
           AppActions.updateItem(a[i].id,time,title);
-          a[i].checked = false;
         }
+       }
+      }
     }
   },
   render: function(){
       var time = this.props.time;
+      var aId = [];
+      for (var i = 0; i < time.length; i++)
+        aId[i] = time[i] + '_id';
     return (
       <div className="button-group">
-        <a className="success button" onClick={this.setTime.bind(this,time[0])}>{time[0]}</a>
-        <a className="success button" onClick={this.setTime.bind(this,time[1])}>{time[1]}</a>
-        <a className="success button" onClick={this.setTime.bind(this,time[2])}>{time[2]}</a>
+        <a className="success button" id={aId[0]} onClick={this.setTime.bind(this,time[0])}>{time[0]}</a>
+        <a className="success button" id={aId[1]} onClick={this.setTime.bind(this,time[1])}>{time[1]}</a>
+        <a className="success button" id={aId[2]} onClick={this.setTime.bind(this,time[2])}>{time[2]}</a>
       </div>
     )
     }
